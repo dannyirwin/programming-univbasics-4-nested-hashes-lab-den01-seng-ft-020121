ed77ec0f40ca7e1218f669f6238d7749bee487cf
Daniel Irwin <dirwin123@gmail.com>
2021-01-09 19:35:47 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -38,6 +38,8 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
+    
+    programmer_hash[:alan_kay][:known_for]
 
 end
 
@@ -57,6 +59,8 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
+    
+    programmer_hash[:dennis_ritchie][:languages]
 
 end
 
